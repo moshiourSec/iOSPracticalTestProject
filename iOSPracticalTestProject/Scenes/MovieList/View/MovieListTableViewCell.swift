881e0e7a1239163a9b006283f2f808ceafedbd5f
Moshiour Rahman <errorlife1995@gmail.com>
2021-05-17 22:58:14 +0600

    update view model and parsed data

---
+++ iOSPracticalTestProject/Scenes/MovieList/View/MovieListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListTableViewCell.swift
+//  iOSPracticalTestProject
+//
+//  Created by Moshiur Rahman Bilash on 5/17/21.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
